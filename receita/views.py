f38d5d06dbdfcf7a28a82d8be4c395f724aa049e
Misael <a95171224@rn.senac.local>
2024-09-05 15:26:02 -0300

    PROJETO01

---
+++ receita/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.html import JsonResponse # type: ignore
+
+# Create your views here.
+
+def tarefa(request):
+    if request.method == 'GET':
+        tarefa = {'id': 1234, 'atividade': 'Estudar para o curso Backend'}
+        return JsonResponse(tarefa)
+
+
